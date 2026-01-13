f634d0ef134b2c0e41c92645d6d10ac523358ef5
US <US@DESKTOP-FLULSU8>
2026-01-13 18:49:34 +0800

    Updated Leader object

--- src/project/roles/Leader.java
+++ src/project/roles/Leader.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class Leader extends User {  
     
     public ArrayList<Lecturer> leaderTeam = new ArrayList<Lecturer>();
-    public ArrayList<Module> Leader_Modules = new ArrayList<Module>();
             
     public Leader (String[] userData) {
         super(userData);
