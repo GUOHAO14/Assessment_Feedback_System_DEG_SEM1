077193d05524544ced300bd493ea013d803fd3c0
US <US@DESKTOP-FLULSU8>
2026-01-21 00:16:49 +0800

    Updated Leader Role

--- src/project/roles/Leader.java
+++ src/project/roles/Leader.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Leader extends User {  
     
     public ArrayList<Lecturer> leaderTeam = new ArrayList<Lecturer>();
-            
+//    public ArrayList<String> createdModules = new ArrayList<>();
+        
     public Leader (String[] userData) {
         super(userData);
     }
@@ -19,5 +20,22 @@ public class Leader extends User {
         String [] fullData = {getId(), getName(), getEmail(), getPW(), getRole()};
         System.out.println(String.join(", ", fullData));
     }
+      public void printLeaderTeam() {
+    if (leaderTeam.isEmpty()) {
+        System.out.println("Leader team is empty.");
+        return;
+    }
+
+    System.out.println("Leader Team Members:");
+    for (Lecturer l : leaderTeam) {
+        System.out.println(l.getId() + " - " + l.getName());
+    }
     
 }
+//      public void addCreatedModule(String moduleId){
+//    if(!createdModules.contains(moduleId)){
+//        createdModules.add(moduleId);
+//    }
+}
+
+
