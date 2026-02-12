68ad7d8cb142f7a1531b969c5f19fc6fa9b62148
GUOHAO14 <guohao.k@hotmail.com>
2026-02-12 19:04:17 +0800

    fix UI

--- src/project/gui/lecturer/DesignAssessment1.java
+++ src/project/gui/lecturer/DesignAssessment1.java
@@ -278,7 +278,7 @@ public class DesignAssessment1 extends FrameFormat {
                         int intFullMarks = Integer.parseInt(fullMarks);
                         
                         // value checking
-                        if (name.length() < Constants.ITEM_NAME_MAX_LENGTH || name.length() > Constants.ITEM_NAME_MAX_LENGTH) throw new IntegerRangeException("Assessment name length", Constants.ITEM_NAME_MIN_LENGTH, Constants.ITEM_NAME_MAX_LENGTH);
+                        if (name.length() < Constants.ITEM_NAME_MIN_LENGTH || name.length() > Constants.ITEM_NAME_MAX_LENGTH) throw new IntegerRangeException("Assessment name length", Constants.ITEM_NAME_MIN_LENGTH, Constants.ITEM_NAME_MAX_LENGTH);
                         if (intPercent < 1 || intPercent > 100) throw new IntegerRangeException("Score Percentage", 1, 100);
                         if (intFullMarks < 1 || intFullMarks > 100) throw new IntegerRangeException("Full Marks", 10, 100);
                         
